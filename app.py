6968f7f3cfab7ad938e9716e73a93792736eac6e
neandersonmaozinha <a.dearaujooliveira@gmail.com>
2024-12-08 20:31:00 -0300

    Ajuste do menu

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ from datetime import datetime
 print(datetime.now().strftime('%d/%m/%Y, %H:%M'))
 print('Menu Principal\n')
 
-print('1. Cadastrar aluno')
-print('2. Listar alunos')
-print('3. Atualizar aluno')
-print('4. Exlcuir alunos')
+print('1. Cadastrar cliente')
+print('2. Listar clientes')
+print('3. Atualizar cliente')
+print('4. Exlcuir cliente')
 print('5. Sair\n')
 
 opcao_escolhida = input('Digite uma opção: ')
 print(f'você escolheu a opção: {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar cliente')
+elif opcao_escolhida == 2:
+    print('Listar clientes')
+elif opcao_escolhida == 3:
+    print('Atualizar cliente')
+elif opcao_escolhida == 4:
+    print('Atualizar cliente')
+else:
+    print('Finalizar programa')
